b51350aff2565bd20a6ee6aba90163894c671410
Student <student@Admins-MacBook-Air.local>
2018-07-09 15:12:54 -0400

    first challange

--- hello.js
+++ hello.js
@@ -1,1 +1,7 @@
-// Author: FirstName LastName
+// Author: Nalani Gomez-Curiel
+console.log("Hello World!");
+console.log("It's a beautiful day for programming, isn't it?");
+console.log("Hey! What's 1 + 1?");
+console.log("It's " + 1 + 1 + " of course!");
+console.log("No, it's " + (1 + 1) + ", silly goose!");
+// console.log("This isn't going to get printed, is it?");
