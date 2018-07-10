585be0cedf1b534db4263be408b41b52c80384f9
Student <student@Admins-MacBook-Air.local>
2018-07-10 14:01:32 -0400

    easter

--- easter-predictor.js
+++ easter-predictor.js
@@ -1,1 +1,47 @@
-// Author: FirstName LastName
+// Author: Nalani Gomez-curiel
+let y = 2015;
+console.log("y = " +y);
+
+let a = y%19;
+console.log("a ="+a);
+
+let b = Math.floor ((y/100));
+console.log("b = " +b);
+
+let c = (y % 100);
+console.log("c =" + c);
+
+let d = b/ 4;
+console.log("d= " +d);
+
+let e =(b % 4);
+console.log ("e =" + e);
+
+let f = Math.round((b+8) / 25);
+console.log("f= " + f);
+
+let g = Math.floor((b-f +1) /3);
+console.log("g=" +g);
+
+let h = (19 * a + b - d - g +15) %30;
+console.log("h=" +h);
+
+let i = Math.floor(c/4);
+console.log("i="+i);
+
+let k = (c%4);
+console.log("k=" +k);
+
+let r = (32 + 2 * e + 2 * i - h - k)%7;
+console.log("r=" + r);
+
+let m = Math.floor((a + 11 * h + 22 * r) / 451);
+console.log("m=" + m);
+
+let n = Math.floor((h + r - 7 * m + 114)/31);
+console.log("n=" + n);
+
+let p =(h + r - 7 * m + 114)%31;
+console.log("p="+p);
+
+console.log("In "+y+", Easter was in "+n+", "+(p+1));
--- hello.js
+++ hello.js
@@ -5,3 +5,8 @@ console.log("Hey! What's 1 + 1?");
 console.log("It's " + 1 + 1 + " of course!");
 console.log("No, it's " + (1 + 1) + ", silly goose!");
 // console.log("This isn't going to get printed, is it?");
+
+// ex for using this is racking points up in video game
+let num1 = 3;
+let num1 + num2;
+let num1 = num1 + num2;
